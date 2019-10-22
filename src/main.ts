6dd731b0934fc9c767170f845e3cccff6237bb6a
andyfeely <andyfeely@gmail.com>
2019-10-22 22:11:58 +0100

    Fixed typescropt shite

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,32 @@ const config = require('./aws-exports').default;
 
 Vue.config.productionTip = false;
 
-Amplify.configure(config);
+// AMPLIFY UPDATE FOR PROD DEPLOYS
+const urlsIn = config.oauth.redirectSignIn.split(',');
+const urlsOut = config.oauth.redirectSignOut.split(',');
+const oauth = {
+  domain: config.oauth.domain,
+  scope: config.oauth.scope,
+  redirectSignIn: config.oauth.redirectSignIn,
+  redirectSignOut: config.oauth.redirectSignOut,
+  responseType: config.oauth.responseType,
+};
+const hasLocalhost = (hostname: string) => Boolean(hostname.match(/localhost/) || hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/));
+const hasHostname = (hostname: string) => Boolean(hostname.includes(window.location.hostname));
+const isLocalhost = hasLocalhost(window.location.hostname);
+if (isLocalhost) {
+  urlsIn.forEach((e: string) => { if (hasLocalhost(e)) { oauth.redirectSignIn = e; } });
+  urlsOut.forEach((e: string) => { if (hasLocalhost(e)) { oauth.redirectSignOut = e; } });
+} else {
+  urlsIn.forEach((e: string) => { if (hasHostname(e)) { oauth.redirectSignIn = e; } });
+  urlsOut.forEach((e: string) => { if (hasHostname(e)) { oauth.redirectSignOut = e; } });
+}
+const configUpdate = config;
+configUpdate.oauth = oauth;
+// AMPLIFY CONFIG DONE
+Amplify.configure(configUpdate);
+
+
 Vue.use(AmplifyPlugin, AmplifyModules);
 Vue.use(VueMateria);
 const apolloConfig = {
